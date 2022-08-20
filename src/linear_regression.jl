0aa8f1a27410ae3f19ee6a09d6f34a3c138361d4
water-dropwort <polarbear.rutile@icloud.com>
2022-08-20 21:49:20 +0900

    rename regression -> linear_regression

--- src/linear_regression.jl
+++ src/linear_regression.jl
@@ -1,7 +1,7 @@
 using LinearAlgebra
 
 # NxKの説明変数の行列とN次元の被説明変数のベクトルを受けとり、K次元のパラメータのベクトルを返す。
-function regression(xmat::Matrix{T}, yvec::Vector{X}) where {T,X}
+function linear_regression(xmat::Matrix{T}, yvec::Vector{X}) where {T,X}
     # 行列の1列目のベクトルの要素数とyvecの要素数の比較。
     if length(xmat[:,1]) != length(yvec)
         @error "Arguments are NxK matrix and N dims vector."
