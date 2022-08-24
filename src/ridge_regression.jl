e8aeacc5cfcb1baf8f37fd1ee57423423c350cde
water-dropwort <polarbear.rutile@icloud.com>
2022-08-24 21:12:51 +0900

    fixed bug

--- src/ridge_regression.jl
+++ src/ridge_regression.jl
@@ -2,6 +2,7 @@ using LinearAlgebra
 # リッジ回帰
 function ridge_regression(X::Matrix{T}, y::Vector{U}, α::V)  where {T,U,V}
     X_T = transpose(X)
-    Imat = Matrix{Int64}(I,length(y),length(y)) # 単位行列
+    k = length(X[1,:])
+    Imat = Matrix{Int64}(I,k,k) # 単位行列
     return inv(X_T * X + α*Imat) * X_T * y
 end
